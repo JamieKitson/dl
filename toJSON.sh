a9de9867e048f8f689434fa062e3e65546a07215
Jamie Kitson <jamie@kitten-x.com>
2012-06-28 18:26:05 +0200

    Update master

--- toJSON.sh
+++ toJSON.sh
@@ -5,12 +5,16 @@ echo '['
 for i in *.txt
 do
 
+    if [ -z "$d" ] 
+    then
+        c=","
+    fi
 	d=$(basename $i -count.txt)
 	n=$(cat $i | sort | uniq | wc -l)
 
 # TODO how do you remove last trailling comma so the JSON is 100%?
 cat <<END
-{ "period" : "$d", "dl" : $n },
+$c{ "period" : "$d", "dl" : $n }
 END
 done
 
